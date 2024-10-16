c98fb095b690277a8ae8443344a392402182d676
mhnash <nasir.shuvo@tuni.fi>
2024-10-16 06:49:44 +0300

    done

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const app = express();
 
 
 app.use(express.json()); 
-app.use(cors()); 
+app.use(cors({
+  origin: ['http://localhost:5173',''],
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Access-Control-Allow-Origin: localhost:5173'],
+})); 
 
 
 
