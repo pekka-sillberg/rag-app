2d7234d0ab22a601fd8aa1c30efccfb4e0c592a1
mhnash <nasir.shuvo@tuni.fi>
2024-11-12 07:14:17 +0200

    browser added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { FaTimes } from 'react-icons/fa';  // Using React Icons for Close Button
+import { FaTimes } from 'react-icons/fa';
 
-
-function Sidebar({ setInput, isOpen, toggleSidebar, openModal }) {
+function Sidebar({ setInput, handleSendMessage, isOpen, toggleSidebar }) {
   const handleQuestionClick = (event) => {
-    const question = event.target.textContent;  // Get the clicked question text
+    const question = event.target.textContent;
     setInput(question);
+    handleSendMessage(question);  // Trigger the API call after setting input
+    setInput('');  // Clear the input after sending the message
     toggleSidebar();
-};
+  };
 
   return (
     <nav id="sidebar" className={isOpen ? 'open' : ''}>
@@ -15,31 +16,18 @@ function Sidebar({ setInput, isOpen, toggleSidebar, openModal }) {
         <div className="logo">
           <h2>Guide GPT</h2>
           <button onClick={toggleSidebar}>
-            <FaTimes/>
+            <FaTimes />
           </button>
         </div>
-
-        {/* <div className='faq'>
-          <h4>FAQ</h4>
-          <hr />
-        </div> */}
         <ul>
           <li onClick={handleQuestionClick}>How can I obtain support for studying and well-being at Tampere University?</li>
-
           <li onClick={handleQuestionClick}>What resources are available for distance learning study skills?</li>
-
           <li onClick={handleQuestionClick}>How can I access additional resources for communication and language studies?</li>
-
           <li onClick={handleQuestionClick}>What support is offered for students with difficulties in learning?</li>
-
           <li onClick={handleQuestionClick}>How can I prepare for exams effectively at Tampere University?</li>
-
           <li onClick={handleQuestionClick}>What services and regulations are in place for students at Tampere University?</li>
-
           <li onClick={handleQuestionClick}>How can I benefit from internationalization during my studies at Tampere University?</li>
-
           <li onClick={handleQuestionClick}>What options are available for internships and work life experiences at Tampere University?</li>
-
         </ul>
       </div>
       <div className="footer">
